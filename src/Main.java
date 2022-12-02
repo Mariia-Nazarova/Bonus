0546a1bc5365c14616028046ec80dacf1c01c7f0
Mariia Nazarova <Djungary@mail.ru>
2022-12-02 10:00:26 +0300

    code

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         double priceTicket = 3540.80; //цена билета, руб
         int priceMile = 20; //цена бонусной мили, руб
 
-        int bonus = (int) priceTicket/priceMile;
+        int bonus = (int) priceTicket / priceMile;
 
         System.out.println("Начислено " + bonus + " бонусных миль");
 
