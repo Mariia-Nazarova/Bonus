43a453c176d3f5d6e6308315070658373f5bbdc6
Mariia Nazarova <Djungary@mail.ru>
2022-12-02 09:53:01 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        double priceTicket = 3540.80; //цена билета, руб
+        int priceMile = 20; //цена бонусной мили, руб
+
+        int bonus = (int) priceTicket/priceMile;
+
+        System.out.println("Начислено " + bonus + " бонусных миль");
+
+    }
+
+}
